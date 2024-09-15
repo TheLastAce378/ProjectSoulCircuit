e08cb355cbd46465f4d4b2a0461bb09c6da3e1cd
TheLastAce378 <80765004+TheLastAce378@users.noreply.github.com>
2024-09-15 05:01:27 +0100

    Add files via upload

---
+++ src/main/kotlin/soulcircuit/ygo/SoulCircuitYGODataGenerator.kt
@@ -0,0 +1,9 @@
+package soulcircuit.ygo
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object SoulCircuitYGODataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
